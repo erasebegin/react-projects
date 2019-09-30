37a596c8c113f4d4799d574bc3953d98354e5be4
erasebegin <chris.james.haupt@gmail.com>
2019-09-30 15:49:05 +0200

    refactor app component to class component

--- image-search/src/components/App.js
+++ image-search/src/components/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 // import ImageList from './components/ImageList'
 import SearchBar from './SearchBar'
 
-const App = () => {
-
-    return(
-        <div className="ui container">
-            <SearchBar />
-        </div>
-    )
-    
+class App extends React.Component {
+    render() {
+        return(
+            <div className="ui container">
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
 export default App;
